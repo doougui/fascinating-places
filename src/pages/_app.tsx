869b7b4f6aed218a991a604bb99ab0c66571abaa
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-12-24 20:29:52 -0300

    Adding raect-leaflet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="img/icon-512.png" />
         <link rel="apple-touch-icon" href="img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092b" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
         <meta
           name="description"
           content="A simple project to show my favorite spots in the world"
