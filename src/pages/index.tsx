2c73892652f76f3e8112ad9295dc2122411e5fc2
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-12-24 21:18:56 -0300

    Creating LinkWrapper component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,5 @@ import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return (
-    <Map
-      places={[
-        {
-          id: '1',
-          name: 'Jaguaruna',
-          slug: 'jaguaruna',
-          location: {
-            latitude: 45,
-            longitude: -10
-          }
-        }
-      ]}
-    />
-  )
+  return <Map />
 }
